85da8f8ff093e38877798bff07ef063012949c94
Matthew Masiello <sotek222@yahoo.com>
2020-02-19 15:17:02 -0500

    adds new adapter instance as prop to Desktop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Desktop from "./components/Desktop";
 import { BrowserRouter as Router } from "react-router-dom";
+import APICommunicator from "./services/Adapter-2";
 
 function App() {
+
+  const adapter = new APICommunicator();
+  
   return (
     <Router>
       <div className="App">
-        <Desktop />
+        <Desktop adapter={adapter}/>
       </div>
     </Router>
   );
